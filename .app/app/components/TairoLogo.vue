8c2859209e62ab88bc8aa3d0f630d70ced8e795e
Al1st1c <gustavo.bindandi@gmail.com>
2026-02-16 02:40:21 -0300

    fx logo

--- .app/app/components/TairoLogo.vue
+++ .app/app/components/TairoLogo.vue
@@ -30,9 +30,10 @@ const logoUrlWhite = computed(() => {
 </script>
 
 <template>
-  <img style="width: 130px;" :src="logoUrl" alt="Logo" class="dark:hidden object-contain max-h-full max-w-full"
-    @error="(e: any) => e.target.src = '/img/logo.png'">
-  <img style="width: 130px;" :src="logoUrlWhite" alt="Logo"
-    class="hidden dark:block object-contain max-h-full max-w-full"
-    @error="(e: any) => e.target.src = '/img/logo-white.png'">
+  <div class="flex items-center w-full max-w-[140px] h-10 overflow-hidden">
+    <img :src="logoUrl" alt="Logo" class="dark:hidden object-contain max-h-full max-w-full w-auto h-auto"
+      @error="(e: any) => e.target.src = '/img/logo.png'">
+    <img :src="logoUrlWhite" alt="Logo" class="hidden dark:block object-contain max-h-full max-w-full w-auto h-auto"
+      @error="(e: any) => e.target.src = '/img/logo-white.png'">
+  </div>
 </template>
