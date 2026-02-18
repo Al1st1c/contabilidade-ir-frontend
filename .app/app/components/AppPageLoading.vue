948a48be6db3061966f4c9aa7b8232a9efc7d363
Al1st1c <gustavo.bindandi@gmail.com>
2026-02-18 13:16:49 -0300

    fxx

--- .app/app/components/AppPageLoading.vue
+++ .app/app/components/AppPageLoading.vue
@@ -12,7 +12,7 @@ withDefaults(defineProps<Props>(), {
 
 <template>
   <div class="flex flex-col items-center justify-center p-8 transition-all duration-300" :style="{ minHeight }">
-    <video src="/img/loading.webm#t=3" autoplay loop muted playsinline class="size-48 mb-0" />
+    <video src="/img/loading.gif#t=3" autoplay loop muted playsinline class="size-48 mb-0" />
     <BaseParagraph v-if="message" size="sm" class="text-muted-500 font-medium animate-pulse">
       {{ message }}
     </BaseParagraph>
